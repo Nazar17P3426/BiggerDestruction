5861c64e80f5146c8e657fa3829016a42b90cfcf
Drmies <141207441+Hugopako@users.noreply.github.com>
2024-05-12 19:48:07 +0700

    Add files via upload

---
+++ DETCEJENIW_private.h
@@ -0,0 +1,23 @@
+/* THIS FILE WILL BE OVERWRITTEN BY DEV-C++ */
+/* DO NOT EDIT ! */
+
+#ifndef DETCEJENIW_PRIVATE_H
+#define DETCEJENIW_PRIVATE_H
+
+/* VERSION DEFINITIONS */
+#define VER_STRING	"6.6.6.6"
+#define VER_MAJOR	6
+#define VER_MINOR	6
+#define VER_RELEASE	6
+#define VER_BUILD	6
+#define COMPANY_NAME	"Hugopako"
+#define FILE_VERSION	"6.6.6.6"
+#define FILE_DESCRIPTION	"Horror Drowning Bunny 6 setup"
+#define INTERNAL_NAME	"Bigger Destroy"
+#define LEGAL_COPYRIGHT	""
+#define LEGAL_TRADEMARKS	""
+#define ORIGINAL_FILENAME	""
+#define PRODUCT_NAME	"The Old Vietnamese Lab"
+#define PRODUCT_VERSION	"6.6.6.6"
+
+#endif /*DETCEJENIW_PRIVATE_H*/
---
+++ PLS README.txt
@@ -0,0 +1,10 @@
+ ###############
+&BiggerDestruction&
+ ###############
+Your PC will die after this
+Trojan: Noskid
+Type: Destructive, i guess
+Works Best In Windows XP
+it also overwrites your MBR to Make your PC unbootable
+If you reboot, Then your MBR died.
+DONT RUN ON REAL MACHINE AND RUN ONLY IN VM!
